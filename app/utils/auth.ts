5e65502bb162b787f4934a11ec91a7c274182b9a
harry <harrybsteele@gmail.com>
2024-03-21 14:51:50 -0400

    removed email provider

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -12,35 +12,6 @@ export const AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    CredentialsProvider({
-      name: "Sign in",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "example@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req): Promise<any> {
-        if (!credentials || !credentials.email || !credentials.password)
-          return null;
-
-        const dbUser = await prisma.user.findFirst({
-          where: { email: credentials.email },
-        });
-
-        if (
-          dbUser &&
-          (await bcrypt.compare(dbUser.password, credentials.password))
-        ) {
-          const { password, ...dbUserWithoutPassowrd } = dbUser;
-          return dbUserWithoutPassowrd;
-        }
-
-        return null;
-      },
-    }),
   ],
   secret: process.env.SECRET,
 } satisfies NextAuthOptions;
